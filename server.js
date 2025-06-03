131f1cd0830fb2ae3d4435de7ec60b7c244d9013
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-03 17:42:22 +0300

    working on the db

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("colors");
 
 const path = require("path");
 const dotenv = require("dotenv");
+const app = require('./app');
+const connectDb = require("./config/db");
+
 
 const pathToEnv = path.join(__dirname, "config", ".env");
 dotenv.config({ path: pathToEnv });
