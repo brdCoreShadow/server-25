fe0a6095294a10ae7eb99f22e9a27e049ee3a8bd
Oleksandr-Berd <alex.berd86@gmail.com>
2025-08-09 19:55:25 +0300

    made getByParams

--- controllers/time-tracking-junior/time-tracking-junior.js
+++ controllers/time-tracking-junior/time-tracking-junior.js
@@ -14,20 +14,34 @@ const getAll = async () => {
   }
 };
 
-const getDaily = async () => {
+const getAllByParams = async (timeframe) => {
   try {
-    const [rows] = await pool.query(`
-    SELECT id, user_name, activity, timeframe, current_hours, previous_hours
-    FROM time_tracking
-  `);
+    let query = ` SELECT id, user_name, activity, timeframe, current_hours, previous_hours
+    FROM time_tracking`;
+
+    if (timeframe === "daily") {
+      query += " WHERE timeframe = 'daily'";
+    
+    } else if (timeframe === "weekly"){
+        query += " WHERE timeframe = 'weekly'"
+       
+    } else if (timeframe === "monthly"){
+        query += " WHERE timeframe = 'monthly'"
+       
+    } else {
+        throw new Error("Invalid filter type");
+    }
+
+    const [rows] = await pool.query(query);
 
     return { data: rows };
   } catch (error) {
-    console.error("Error fetching data", error);
+    console.error("Error fetching daily data", error);
     throw error;
   }
 };
 
 module.exports = {
   getAll,
+  getAllByParams
 };
--- routes/time-tracking-junior/time-tracking-juniorRoutes.js
+++ routes/time-tracking-junior/time-tracking-juniorRoutes.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const router = express.Router()
-const timeTrackingModel = require('../../controllers/time-tracking-junior/time-tracking-junior');
-
+const express = require("express");
+const router = express.Router();
+const timeTrackingModel = require("../../controllers/time-tracking-junior/time-tracking-junior");
 
 router.get("/", async (req, res) => {
+  try {
+    const data = await timeTrackingModel.getAll();
 
-try {
-    
-const data = await timeTrackingModel.getAll()
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching tracker", error);
 
-res.json(data)
+    res.status(500).json({ message: `Server error: ${error}` });
+  }
+});
 
-} catch (error) {
-    console.error("Error fetching tracker", error)
+router.get("/:timeframe", async (req, res) => {
+  try {
+    const data = await timeTrackingModel.getAllByParams(req.params.timeframe);
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching tracker", error);
 
-    res.status(500).json({message:`Server error: ${error}`})
-}
+    res.status(500).json({ message: `Server error: ${error}` });
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
